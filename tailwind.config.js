0bf71b39ad71ee90143576ca09c8ba9e010207d2
zzVu77 <nguyenvanvu24062004@gmail.com>
2024-11-12 15:30:23 +0700

    feat: add comment component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  content: ["./views/**/*.hbs"],
-  prefix: "tw-",
+  content: [
+    './views/**/*.hbs', // Đảm bảo Tailwind đọc được các file handlebars trong thư mục views
+    './public/**/*.html', // Đường dẫn tới các file HTML hoặc các file cần thiết khác
+    './src/**/*.{js,jsx,ts,tsx}', // Đường dẫn tới các file JavaScript nếu cần],
+    './public/**/*.css',
+  ],
+  prefix: 'tw-',
   important: true,
   corePlugins: {
     preflight: false,

bc734bfe01a9faaff6d9364cbc8f15b476a40de9
zzVu77 <129610868+zzVu77@users.noreply.github.com>
2024-11-12 11:32:51 +0700

    Revert "Feat/post card"

--- main.js
+++ main.js
@@ -1,48 +1,35 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import { engine } from "express-handlebars";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-// const path = require("path");
-
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 app.engine(
-  'hbs',
+  "hbs",
   engine({
-    extname: 'hbs',
-    defaultLayout: 'main',
-    layoutsDir: join(__dirname, '/views/layouts/'),
-    partialsDir: join(__dirname, '/views/components/'),
+    extname: "hbs",
+    defaultLayout: "main",
   })
 );
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
-app.set('view engine', 'hbs');
-app.set('views', './views/pages');
-app.use(express.static('public'));
-
-app.get('/posts', function (req, res) {
-  res.render('posts');
-});
-app.get('/article', function (req, res) {
-  res.render('article-detail');
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get('/features', function (req, res) {
-  res.render('features');
+app.get("/features", function (req, res) {
+  res.render("features");
 });
-app.get('/about', function (req, res) {
-  res.render('about');
-});
-
-app.get('/admin', function (req, res) {
-  res.render('admin/dashboard', { layout: 'admin', title: 'Admin Dashboard' });
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
-  console.log('ecApp is running at http://localhost:3000');
+  console.log("ecApp is running at http://localhost:3000");
 });
--- views/components/admin/sidebar/sidebar.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.second-button').addEventListener('click', function () {
-
-document.querySelector('.animated-icon2').classList.toggle('open');
-});

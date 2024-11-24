ea76f5d0dba100f3e53409af54d91b05481ea789
giahuynguyen28 <giahuy902@gmail.com>
2024-11-24 14:11:01 +0700

    add new route

--- main.js
+++ main.js
@@ -38,8 +38,26 @@ app.get('/features', function (req, res) {
 app.get('/about', function (req, res) {
   res.render('about');
 });
-app.get('/article-editor', function (req, res) {
-  res.render('article-editor');
+app.get('/article-writer-textEditor', function (req, res) {
+  res.render('article-writer-textEditor');
+});
+app.get('/article-writer-editTextEditor', function (req, res) {
+  res.render('article-writer-editTextEditor');
+});
+app.get('/article-manage-approved', function (req, res) {
+  res.render('article-manage-approved');
+});
+app.get('/article-manage-pending', function (req, res) {
+  res.render('article-manage-pending');
+});
+app.get('/article-manage-published', function (req, res) {
+  res.render('article-manage-published');
+});
+app.get('/article-manage-rejected', function (req, res) {
+  res.render('article-manage-rejected');
+});
+app.get('/article-manage-all', function (req, res) {
+  res.render('article-manage-all');
 });
 app.get('/admin', function (req, res) {
   res.render('admin/dashboard', { layout: 'admin', title: 'Admin Dashboard' });

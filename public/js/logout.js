91170496ec37af9157c5cadaf732a035777df41f
Manh Cuong Kim <kimmanhcuong230304@gmail.com>
2024-12-24 19:56:38 +0700

    feat: update logic for UI

---
+++ public/js/logout.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const logoutButton = document.querySelector('.client-header-loginButton');
+
+    if (logoutButton) {
+        logoutButton.addEventListener('click', function () {
+            fetch('/auth/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    if (response.ok) {
+                        window.location.href = '/login';
+                    } else {
+                        console.error('Logout failed');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        });
+    }
+});

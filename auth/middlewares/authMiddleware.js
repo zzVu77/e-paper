f451ae475a84f94f869030c01263f4147f2cc31b
giahuynguyen28 <giahuy902@gmail.com>
2024-12-25 19:20:07 +0700

    add admin role in ensureWriter in middleware

--- auth/middlewares/authMiddleware.js
+++ auth/middlewares/authMiddleware.js
@@ -1,22 +1,22 @@
 function ensureAuthenticated(req, res, next) {
-    if (req.user) {
-        return next();
-    }
-    res.redirect("/login");
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
 function ensureAdmin(req, res, next) {
-    if (req.user && req.user.role === "admin") {
-        return next();
-    }
-    res.status(403).json({ error: "Forbidden. Admins only." });
+  if (req.user && req.user.role === "admin") {
+    return next();
+  }
+  res.status(403).json({ error: "Forbidden. Admins only." });
 }
 
 function ensureWriter(req, res, next) {
-    if (req.user && (req.user.role === "writer")) {
-        return next();
-    }
-    res.render("404");
+  if (req.user && (req.user.role === "writer" || req.user.role === "")) {
+    return next();
+  }
+  res.render("404");
 }
 
 export default { ensureAuthenticated, ensureAdmin, ensureWriter };
